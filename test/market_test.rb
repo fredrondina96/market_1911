5b4cf90b7be0d84e5352e04b0922405a33ffc8f1
Fred Rondina <fredrondina96@gmail.com>
2020-01-14 09:34:17 -0700

    Added attribute name and test

--- test/market_test.rb
+++ test/market_test.rb
@@ -14,4 +14,8 @@ class MarketTest < Minitest::Test
   def test_market_exists
     assert_instance_of Market, @market
   end
+
+  def test_market_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
 end
