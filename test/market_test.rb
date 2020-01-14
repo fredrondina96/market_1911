3fd75eba79996b84e6835ae06675bf2b3eb3e429
Fred Rondina <fredrondina96@gmail.com>
2020-01-14 09:32:46 -0700

    Added market class creation and initial test

---
+++ test/market_test.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/item'
+require_relative '../lib/vendor'
+require_relative '../lib/market'
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_market_exists
+    assert_instance_of Market, @market
+  end
+end
